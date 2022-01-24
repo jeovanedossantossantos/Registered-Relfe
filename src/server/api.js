9c13c33369c7dcfcd9487a8af827aa5faecd5e9f
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-01-24 09:35:33 -0300

    Iniciando o desenvovimento

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
     baseURL: process.env.REACT_APP_API,
-    // 'https://sheetdb.io/api/v1/kb2wnmo78wvfp',
+    
     auth: {
         username: process.env.REACT_APP_USERNAME,
         password: process.env.REACT_APP_PASSWORD
