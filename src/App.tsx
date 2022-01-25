75f19deb86535f5e8cb1402a35abc57a394984de
Lucas Garcia <lucasgarcia561@gmail.com>
2022-01-24 21:08:58 -0300

    adicinando listar matriculados

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import Formulario from "./views/Formulario"
 import EditForm from "./views/EditForm"
+import ListaMat from "./views/ListaMat";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Form></Form>
       <Formulario></Formulario>
       <EditForm></EditForm>
+      <ListaMat></ListaMat>
     </div>
   );
 }
---
+++ src/views/ListaMat/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react' 
+
+const ListaMat: React.FC = () => {
+    return (
+        <div>
+            Teste 
+        </div>
+    )
+}
+export default ListaMat
