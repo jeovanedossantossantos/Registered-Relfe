3f915d7bb3f14904c75515b7f9a46c29e7d64f50
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-01-24 22:05:27 -0300

    Ajustes de dodos

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useCallback, useState } from "react";
+import React, { FormEvent, useCallback, useEffect, useState } from "react";
 import api from "../../server/api"
 import { Container } from "./style";
 
@@ -6,35 +6,46 @@ import { Container } from "./style";
 
 interface IDate {
 
+
+    data_cadastro: {
+        dia: number,
+        mes: number,
+        ano: number
+    };
     nome: string;
+    periodo: string;
+    grupo: number;
+    escola: string;
+    cras: string;
     dataNascimento: string;
     certidaoNascimento?: string;
     corRaca: string;
     cpf: string;
-    sexo: "Não indentificado" | "Masculino" | "Feminino";
+    sexo: string;
     nis?: string;
     endereco: string;
     numero: string;
     bairro: string;
     pontoReferencia: string;
     telefone: string;
+    telefone2: string;
     cidade: string;
     cep: string;
     tipoSanguinio?: string;
-    restriacoAlimetar: "Não" | "Sim";
-    qualRestriacoAlimetar?: string;
-    medicamento: "Não" | "Sim";
-    qualMedicamento?: string;
-    alergiaMedicamento: "Não" | "Sim";
-    qualAlergiaMedicamento?: string;
-    denca: "Não" | "Sim";
-    qualDenca?: string;
-    deficiencai: "Não" | "Sim";
-    qualDeficiencai?: string;
+    // restriacoAlimetar: "Não" | "Sim";
+    // qualRestriacoAlimetar?: string;
+    // medicamento: "Não" | "Sim";
+    // qualMedicamento?: string;
+    // alergiaMedicamento: "Não" | "Sim";
+    // qualAlergiaMedicamento?: string;
+    // denca: "Não" | "Sim";
+    // qualDenca?: string;
+    // deficiencai: "Não" | "Sim";
+    // qualDeficiencai?: string;
 
-    obs?: string;
+    // obs?: string;
 
-    autoriz: "Não" | "Sim";
+    // autoriz: "Não" | "Sim";
 
     nomePai: string;
     cpfPai: string;
@@ -62,35 +73,52 @@ interface IDate {
 
 const Form: React.FC = () => {
 
-    const [data, setDate] = useState<IDate>({} as IDate)
-    // const hadleSumbmit = useCallback((e: FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault()
-    //     // api.get("/",).then(response =>{
-    //     //     console.log(response.data)
+    const [date, setDate] = useState<IDate>({} as IDate)
 
-    //     // }
-
-    //     api.post("/", data).then(()=>{
-
-    //             console.log(data)
-
-    //     }
-
+    useEffect(() => {
+        const data = new Date();
+        let ca = []
+        ca.push({
+            dia: data.getDate(),
+            mes: data.getMonth(),
+            ano: data.getFullYear()
+        })
+        // alert(ca[0].ano)
+        setDate({ ...date, data_cadastro: ca[0] })
 
 
-    //     ).catch((err) => {
-    //         {
-    //             console.log(data)
-    //         }
-    //     })
+    }, [])
+    console.log(date.data_cadastro)
+    const hadleSumbmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        // api.get("/",).then(response =>{
+        //     console.log(response.data)
 
-    // }, [data])
+        // }
+
+        api.post("/", date).then(() => {
+
+            console.log(date)
+
+        }
+
+
+
+        ).catch((err) => {
+            {
+                console.log(date)
+            }
+        })
+
+    }, [date])
 
     return (
         <div>
 
             formulario
+            <div>
 
+            </div>
             <Container className="card">
                 <h5>Cadastres</h5>
                 {/* onSubmit={hadleSumbmit} */}
@@ -106,72 +134,156 @@ const Form: React.FC = () => {
 
                     <button type="submit" value="Cadastra">Cadastra</button>
                 </form> */}
-                <form>
+                <form onSubmit={hadleSumbmit}>
+
+
                     <fieldset>
                         <legend>Detalhes</legend>
-                        <label htmlFor="nome">Nome:</label>
-                        <input type="text" name="nome" id="nome" />
-                        <label htmlFor="dataNascimento">Data de Nascimento:</label>
-                        <input type="text" name="dataNascimento" id="dataNascimento" />
-                        <label htmlFor="certidao">Certidão de Nascimento:</label>
-                        <input type="text" name="certidao" id="certidao" />
-                        <label htmlFor="raça">Cor/Raça:</label>
-                        <input type="text" name="raça" id="raça" />
-                        <label htmlFor="CPFbeneficiario">CPF Beneficiário:</label>
-                        <input type="text" name="CPFbeneficiario" id="CPFbeneficiario" />
-                        <label htmlFor="RGbeneficiario">RG Beneficiário:</label>
-                        <input type="text" name="RGbeneficiario" id="RGbeneficiario" />
-                        <label htmlFor="sexo">Sexo:</label>
-                        <input type="text" name="sexo" id="sexo" />
-                        <label htmlFor="nis">NIS(CAD ÚNICO)</label>
-                        <input type="text" name="nis" id="nis" />
-                        <label htmlFor="endereco">Endereço:</label>
-                        <input type="text" name="endereco" id="endereco" />
-                        <label htmlFor="numeroEndereco">Nº):</label>
-                        <input type="text" name="numeroEndereco" id="numeroEndereco" />
-                        <label htmlFor="bairro">Bairro:</label>
-                        <input type="text" name="bairro" id="bairro" />
-                        <label htmlFor="cidade">Cidade:</label>
-                        <input type="text" name="cidade" id="cidade" />
-                        <label htmlFor="cep">CEP:</label>
-                        <input type="text" name="cep" id="cep" />
-                        <label htmlFor="referencia">Ponto de Referência:</label>
-                        <input type="text" name="referencia" id="referencia" />
-                        <label htmlFor="telContato">Telefone para Contato:</label>
-                        <input type="text" name="telContato" id="telContato" />
-                        <label htmlFor="recado">Recado:</label>
-                        <input type="text" name="recado" id="recado" />
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="nome">Nome:</label>
+                            <input className="form-control" type="text" name="nome" id="nome" />
+                        </div>
+                        <div style={{display:"flex"}}>
+                            <div className="col-md-8 m-1">
+                                <label className="form-label" htmlFor="escola">Escola:</label>
+                                <input className="form-control" type="text" name="escola" id="escola" />
+                            </div>
+                            <div className="col-md-3 m-1">
+                                <label className="form-label" htmlFor="periodo">Periodo:</label>
+                                <input className="form-control" type="number" name="periodo" id="periodo" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <label htmlFor="dataNascimento">Data de Nascimento:</label>
+                                <input type="number" name="dataNascimento" id="dataNascimento" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="certidao">Certidão de Nascimento:</label>
+                                <input type="number" name="certidao" id="certidao" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="raça">Cor/Raça:</label>
+                                <input type="text" name="raça" id="raça" />
+                            </div>
+                            <div className="col-md-2">
+
+                                <label htmlFor="CPFbeneficiario">CPF Beneficiário:</label>
+                                <input type="number" name="CPFbeneficiario" id="CPFbeneficiario" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="RGbeneficiario">RG Beneficiário:</label>
+                                <input type="number" name="RGbeneficiario" id="RGbeneficiario" />
+                            </div>
+                            <div className="col-md-2">
+                                <label htmlFor="sexo">Sexo:</label><br />
+                                <input type="text" name="sexo" id="sexo" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="nis">NIS(CAD ÚNICO)</label>
+                                <input type="number" name="nis" id="nis" />
+                            </div>
+                        </div>
+
+
+
+                        <div className="row">
+                            <div className="col-md-2" style={{
+                                width: "100%",
+                                maxWidth: 500, minWidth: 200
+                            }}>
+                                <label htmlFor="endereco">Endereço:</label><br />
+                                <input style={{
+                                    width: "100%",
+                                    maxWidth: 480,
+                                    minWidth: 200
+
+                                }} type="text" name="endereco" id="endereco" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="numeroEndereco">Nº):</label><br />
+                                <input type="number" name="numeroEndereco" id="numeroEndereco" />
+                            </div>
+
+                            <div className="col-md-2" style={{
+                                width: "100%",
+                                maxWidth: 500, minWidth: 200
+                            }}>
+                                <label htmlFor="bairro" >Bairro:</label>
+                                <input style={{
+                                    width: "100%",
+                                    maxWidth: 480,
+                                    minWidth: 200
+
+                                }} type="text" name="bairro" id="bairro" />
+                            </div >
+
+                            <div className="col-md-2">
+                                <label htmlFor="cidade">Cidade:</label>
+                                <input type="text" name="cidade" id="cidade" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="cep">CEP:</label> <br />
+                                <input type="number" name="cep" id="cep" />
+                            </div>
+
+                            <div className="col-md-3">
+
+                                <label htmlFor="referencia">Ponto de Referência:</label><br />
+                                <input className="col-md-12" type="text" name="referencia" id="referencia" />
+
+                            </div>
+                            <div className="col-md-2">
+                                <label htmlFor="telContato">Telefone para Contato:</label>
+                                <input type="text" name="telContato" id="telContato" />
+                            </div>
+
+                            <div className="col-md-2">
+                                <label htmlFor="telContato2">Telefone para Contato:</label>
+                                <input type="text" name="telContato2" id="telContato2" />
+                            </div>
+
+
+                        </div>
                     </fieldset>
 
                     <fieldset>
-                        <legend>Dados do Pai</legend>
-                        <label htmlFor="nomePai">Nome do Pai:</label>
+                        {/* <legend>Dados do Pai</legend>
+                        <label htmlFor="nomePai">Nome do Pai:</label> */}
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="nomePai">Dados do Pai/Padastro</label>
+                            <input className="form-control" type="text" name="nomePai" id="nomePai" placeholder="Nome do pai" />
+                        </div>
                         <input type="text" name="nomePai" id="nomePai" />
-                        <label htmlFor="telPai">Telefone do Pai:</label>
+                        <label htmlFor="telPai">Telefone:</label>
                         <input type="text" name="telPai" id="telPai" />
-                        <label htmlFor="CPFpai">CPF do Pai:</label>
+                        <label htmlFor="CPFpai">CPF:</label>
                         <input type="text" name="CPFpai" id="CPFpai" />
-                        <label htmlFor="RGpai">RG do Pai:</label>
+                        <label htmlFor="RGpai">RG:</label>
                         <input type="text" name="RGpai" id="RGpai" />
-                        <label htmlFor="NISpai">NIS do Pai:</label>
-                        <input type="text" name="NISpai" id="NISpai" />
+
                     </fieldset>
 
                     <fieldset>
-                        <legend>Dados da Mãe</legend>
-                        <label htmlFor="nomeMae">Nome da Mãe:</label>
+                        <legend>Dados da Mãe/Madastra</legend>
+                        <label htmlFor="nomeMae">Nome:</label>
                         <input type="text" name="nomeMae" id="nomeMae" />
-                        <label htmlFor="telMae">Telefone da Mãe:</label>
+                        <label htmlFor="telMae">Telefone da:</label>
                         <input type="text" name="telMae" id="telMae" />
-                        <label htmlFor="CPFmae">CPF da Mãe:</label>
+                        <label htmlFor="CPFmae">CPF:</label>
                         <input type="text" name="CPFmae" id="CPFmae" />
-                        <label htmlFor="RGmae">RG da Mãe:</label>
+                        <label htmlFor="RGmae">RG:</label>
                         <input type="text" name="RGmae" id="RGmae" />
-                        <label htmlFor="NISmae">NIS da Mãe:</label>
-                        <input type="text" name="NISmae" id="NISmae" />
+
                     </fieldset>
 
-                    <fieldset>
+                    {/* <fieldset>
                         <legend>Dados do Padrasto/Madrasta</legend>
                         <label htmlFor="nomePadrasto">Nome do Padrasto:</label>
                         <input type="text" name="nomePadrasto" id="nomePadrasto" />
@@ -181,9 +293,9 @@ const Form: React.FC = () => {
                         <input type="text" name="nomeMadrasta" id="nomeMadrasta" />
                         <label htmlFor="telMadrasta">Telefone da Madrasta</label>
                         <input type="text" name="telMadrasta" id="telMadrasta" />
-                    </fieldset>
+                    </fieldset> */}
 
-                    <fieldset>
+                    {/* <fieldset>
                         <legend>Dados do Responsável Legal</legend>
                         <label htmlFor="responsavel">Responsável Legal</label>
                         <input type="text" name="responsavel" id="responsavel" />
@@ -199,9 +311,9 @@ const Form: React.FC = () => {
                         <input type="text" name="telParente" id="telParente" />
                         <label htmlFor="estadoCivil">Estado Civil:</label>
                         <input type="text" name="estadoCivil" id="estadoCivil" />
-                    </fieldset>
+                    </fieldset> */}
 
-                    <fieldset>
+                    {/* <fieldset>
                         <legend>Observações</legend>
                         <label htmlFor="restricaoAlimentar">Apresenta Restrição Alimentar:</label>
                         <input type="text" name="restricaoAlimentar" id="restricaoAlimentar" />
@@ -233,11 +345,13 @@ const Form: React.FC = () => {
                         <input type="text" name="encaminhada" id="encaminhada" />
                         <label htmlFor="beneficiario">Beneficiário Ativo:</label>
                         <input type="checkbox" name="beneficiario" id="beneficiario" />
-                    </fieldset>
+                    </fieldset> */}
                 </form>
-                <button>Cadastrar</button>
-                <button>Cancelar</button>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <button type="submit" className="btn btn-success m-3">Cadastrar</button>
+                    <button className="btn btn-danger  m-3">Cancelar</button>
 
+                </div>
             </Container>
         </div>
     )
