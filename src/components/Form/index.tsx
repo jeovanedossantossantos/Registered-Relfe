64d32abb0d8df80627e22e9387816595c83b3563
Lucas Garcia <lucasgarcia561@gmail.com>
2022-01-28 20:32:24 -0300

    primeiros ajustes no style

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -85,7 +85,7 @@ const redirecionar = ()=>{
         <div>
             <h1>Logo do Relfe</h1>
         
-            <Container className="card">
+            <Container>
                 
                 {/* onSubmit={hadleSumbmit} */}
                 {/* <form >
@@ -106,156 +106,99 @@ const redirecionar = ()=>{
     
     
                         <fieldset>
-                            <legend>Identificação Do Beneficiário</legend>
-                            <div className="mb-3">
-                                <label className="form-label" htmlFor="nome">Nome do Beneficiário</label>
-                                <input  type="text" name="nome" id="nome"
-                                placeholder="Digite o nome do beneficiário"
-                                 onChange={e => setDate({ ...date, nomeBeneficiario: e.target.value })} />
-                            </div>
-    
-                            <div className="row">
-                                <div className="mb-3">
+                                <div className="row">
+                                <legend>Identificação Do Beneficiário</legend>
+                                    <label htmlFor="nome">Nome do Beneficiário</label>
+                                    <input  type="text" name="nome" id="nome" placeholder="Digite o nome do beneficiário" onChange={e => setDate({ ...date, nomeBeneficiario: e.target.value })}/>
+                                    
                                     <label htmlFor="dataNascimento">Data de Nascimento:</label>
-                                    <input type="date" name="dataNascimento" id="dataNascimento" placeholder="Digite a data"
-                                    onChange={e => setDate({ ...date, dataNascimento: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="date" name="dataNascimento" id="dataNascimento" placeholder="Digite a data" onChange={e => setDate({ ...date, dataNascimento: e.target.value })}/>
+                                
                                     <label htmlFor="CPFbeneficiario">CPF Beneficiário:</label>
-                                    <input type="text" name="CPFbeneficiario" id="CPFbeneficiario" 
-                                    placeholder="Digite o CPF"
-                                    onChange={e => setDate({ ...date, cpf: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="CPFbeneficiario" id="CPFbeneficiario" placeholder="Digite o CPF" onChange={e => setDate({ ...date, cpf: e.target.value })}/>
+                                
                                     <label htmlFor="RGbeneficiario">RG Beneficiário:</label>
-                                    <input type="text" name="RGbeneficiario" id="RGbeneficiario" placeholder="Digite o RG"
-                                    onChange={e => setDate({ ...date, rg: e.target.value })} />
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="RGbeneficiario" id="RGbeneficiario" placeholder="Digite o RG" onChange={e => setDate({ ...date, rg: e.target.value })} />
+                                
                                     <label htmlFor="sexo">Sexo:</label><br />
-                                    <input type="text" name="sexo" id="sexo" placeholder="Digite o sexo "
-                                    onChange={e => setDate({ ...date, sexo: e.target.value })} />
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="sexo" id="sexo" placeholder="Digite o sexo " onChange={e => setDate({ ...date, sexo: e.target.value })} />
+                                
                                     <label htmlFor="nis">NIS(CAD ÚNICO)</label>
-                                    <input type="text" name="nis" id="nis" placeholder="Digite o NIS"
-                                    onChange={e => setDate({ ...date, nis: e.target.value })}/>
-                                </div>
-                            </div>
-    
-                            <div className="row">
-                                <div className="mb-3" >
+                                    <input type="text" name="nis" id="nis" placeholder="Digite o NIS" onChange={e => setDate({ ...date, nis: e.target.value })}/>
+                                
                                     <label htmlFor="endereco">Endereço:</label><br />
-                                    <input type="text" name="endereco" id="endereco" placeholder="Digite o endereço"
-                                    onChange={e => setDate({ ...date, endereco: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="endereco" id="endereco" placeholder="Digite o endereço" onChange={e => setDate({ ...date, endereco: e.target.value })}/>
+                               
                                     <label htmlFor="bairro" >Bairro:</label>
-                                    <input type="text" name="bairro" id="bairro" placeholder="Digite o bairro"
-                                    onChange={e => setDate({ ...date, bairro: e.target.value })}/>
-                                </div >
-    
-                                <div className="mb-3">
+                                    <input type="text" name="bairro" id="bairro" placeholder="Digite o bairro" onChange={e => setDate({ ...date, bairro: e.target.value })}/>
+                                
                                     <label htmlFor="referencia">Ponto de Referência:</label><br />
-                                    <input type="text" name="referencia" id="referencia" placeholder="Digite o ponto de referência" 
-                                    onChange={e => setDate({ ...date, pontoReferencia: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="referencia" id="referencia" placeholder="Digite o ponto de referência" onChange={e => setDate({ ...date, pontoReferencia: e.target.value })}/>
+                                
                                     <label htmlFor="periodoRelfe">Período no Relfe:</label>
-                                    <input type="text" name="periodoRelfe" id="periodoRelfe" placeholder="Período no Relfe"
-                                    onChange={e => setDate({ ...date, periodoRelfe: e.target.value })} />
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="periodoRelfe" id="periodoRelfe" placeholder="Período no Relfe" onChange={e => setDate({ ...date, periodoRelfe: e.target.value })} />
+                                
                                     <label htmlFor="telContato">Telefone 1:</label>
-                                    <input type="text" name="telContato" id="telContato" placeholder="Digite o telefone"
-                                    onChange={e => setDate({ ...date, telefone1: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="telContato" id="telContato" placeholder="Digite o telefone" onChange={e => setDate({ ...date, telefone1: e.target.value })}/>
+                                
                                     <label htmlFor="telContato2">Telefone 2:</label>
-                                    <input type="text" name="telContato2" id="telContato2" placeholder="Digite o telefone"
-                                     onChange={e => setDate({ ...date, telefone2: e.target.value })}/>
-                                </div>
-    
-                                <div className="mb-3">
+                                    <input type="text" name="telContato2" id="telContato2" placeholder="Digite o telefone" onChange={e => setDate({ ...date, telefone2: e.target.value })}/>
+                                
                                     <label htmlFor="telefone-recado">Telefone Recado:</label>
-                                    <input type="text" name="telfone-recado" id="telefone-recado" placeholder="Telefone para recado" 
-                                    onChange={e => setDate({ ...date, telFoneRec: e.target.value })}/>
+                                    <input type="text" name="telfone-recado" id="telefone-recado" placeholder="Telefone para recado" onChange={e => setDate({ ...date, telFoneRec: e.target.value })}/>
                                 </div>
+                        </fieldset>
     
+                        <fieldset>
+                            <div className="row">
+                                <legend>Filiação / Responsável Legal</legend>
+                                <label htmlFor="nomePai">Nome do Pai:</label>
+                                <input type="text" name="nomePai" id="nomePai" placeholder="Digite o nome do pai" onChange={e => setDate({ ...date, nomePai: e.target.value })}/>
+
+                                <label htmlFor="nomeMae">Nome da Mãe:</label>
+                                <input type="text" name="nomeMae" id="nomeMae" placeholder="Digite o nome da mãe" onChange={e => setDate({ ...date, nomeMae: e.target.value })}/>
+                            
+                                <label htmlFor="responsavel">Responsável Legal:</label>
+                                <input type="text" name="responsavel" id="responsavel" placeholder="Digite o nome do responsalvel" />
+                            
+                                <label htmlFor="parentesco">Parentesco:</label>
+                                <input type="text" name="parentesco" id="parentesco" placeholder="Digite o parentesco"/>
+                            </div>
+                        </fieldset>
+
+                        <fieldset>
+                            <div className="row">
+                                <legend>Observações Médicas</legend>
+                                <label htmlFor="obs">OBS:</label>
+                                <input type="text" name="obs" id="obs" placeholder="Digite a observação médica"/>
                             </div>
                         </fieldset>
     
-                        <fieldset className="mb-3">
-                        <legend>Filiação / Responsável Legal</legend>
-                            <label htmlFor="nomePai">Nome do Pai:</label>
-                            <input type="text" name="nomePai" id="nomePai" placeholder="Digite o nome do pai"
-                            onChange={e => setDate({ ...date, nomePai: e.target.value })}/>
-                        </fieldset>
-    
-                        <fieldset className="mb-3">
-                            <label htmlFor="nomeMae">Nome da Mãe:</label>
-                            <input type="text" name="nomeMae" id="nomeMae" placeholder="Digite o nome da mãe"
-                            onChange={e => setDate({ ...date, nomeMae: e.target.value })}/>
-                            </fieldset>
-    
-                        <fieldset className="mb-3">
-                            <label htmlFor="responsavel">Responsável Legal:</label>
-                            <input type="text" name="responsavel" id="responsavel" placeholder="Digite o nome do responsalvel" />
-                        </fieldset>
-    
-                        <fieldset className="mb-3">
-                            <label htmlFor="parentesco">Parentesco:</label>
-                            <input type="text" name="parentesco" id="parentesco" placeholder="Digite o parentesco"/>
-                        </fieldset>
-    
                         <fieldset>
-                            <legend>Observações Médicas</legend>
-                            <label htmlFor="obs">OBS:</label>
-                            <input type="text" name="obs" id="obs" placeholder="Digite a observação médica"/>
-                        </fieldset>
-    
-                        <fieldset>
+                            <div className="row">
                             <legend>Histórico Escolar</legend>
-                            <div>
-                                <div className="mb-3">
-                                    <label className="form-label" htmlFor="escola">Escola:</label>
-                                    <input type="text" name="escola" id="escola"
-                                    placeholder="Nome da escola"
-                                    onChange={e => setDate({ ...date, escola: e.target.value })} />
-                                </div>
-                                <div className="mb-3">
-                                    <label className="form-label" htmlFor="periodo">Período da Escola:</label>
-                                    <input type="number" name="periodo" id="periodo" 
-                                    placeholder="Periodo da escola"
-                                    onChange={e => setDate({ ...date, periodoEscola: e.target.value })}/>
-                                </div>
-                                </div>
-                                <div className="mb-3">                             <label htmlFor="serie">Série:</label>
-                                    <input type="text" name="serie" id="serie" placeholder="Digite a série"
-                                    onChange={e => setDate({ ...date, serie: e.target.value })}/>
-                                </div>				
-                                <div className="mb-3">                               <label htmlFor="ano">Ano:</label><br />
-                                    <input type="number" name="ano" id="ano" placeholder="Digite o ano"
-                                    onChange={e => setDate({ ...date, ano: e.target.value })}/>
-                                </div>	
+                                <label className="form-label" htmlFor="escola">Escola:</label>
+                                <input type="text" name="escola" id="escola" placeholder="Nome da escola" onChange={e => setDate({ ...date, escola: e.target.value })} />
+                                
+                                <label className="form-label" htmlFor="periodo">Período da Escola:</label>
+                                <input type="number" name="periodo" id="periodo" placeholder="Periodo da escola" onChange={e => setDate({ ...date, periodoEscola: e.target.value })}/>
+                                
+                                <label htmlFor="serie">Série:</label>
+                                <input type="text" name="serie" id="serie" placeholder="Digite a série" onChange={e => setDate({ ...date, serie: e.target.value })}/>				
+                                                               
+                                <label htmlFor="ano">Ano:</label>
+                                <input type="number" name="ano" id="ano" placeholder="Digite o ano" onChange={e => setDate({ ...date, ano: e.target.value })}/>
+                            </div>	
                         </fieldset>
     
                         <fieldset>
-                            <legend>Relato de Desligamento</legend>
-                            <div className="mb-3">                              <label htmlFor="dataDesligamento">Data de Desligamento</label> <br />
-                                    <input type="date" name="dataDesligamento" id="dataDesligamento" 
-                                    onChange={e => setDate({ ...date, dataDesligamento: e.target.value })}/>
-                                    <label htmlFor="obs">OBS:</label>
-                                    <input type="text" name="obs" id="obs" placeholder="Digite a observação do desligamento"/>
-                                </div>
+                            <div className="row">          
+                            <legend>Relato de Desligamento</legend>                    
+                                <label htmlFor="dataDesligamento">Data de Desligamento</label>
+                                <input type="date" name="dataDesligamento" id="dataDesligamento" onChange={e => setDate({ ...date, dataDesligamento: e.target.value })}/>
+                                <label htmlFor="obs">OBS:</label>
+                                <input type="text" name="obs" id="obs" placeholder="Digite a observação do desligamento"/>
+                            </div>
                         </fieldset>
                         <div style={{ display: "flex", justifyContent: "center" }}>
                         <button type="submit" className="btn btn-success m-3">Cadastrar</button>
--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -1,6 +1,49 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- margin: auto;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 100%
+    color: #040004;
+    line-height: 1.5;
+}
 
+h1{
+    text-align: center;
+    border-radius: 10px;
+    padding: 20px;
+    margin-bottom: 20px;
+    box-shadow: 1px 1px 2px 2px #E91E63;
+    font-family: Verdana;
+    font-weight: bold;
+}
+
+form{
+    background-color: #fff;
+    width: 720px;
+    padding: 20px;
+    margin: 20px auto 0 auto;
+    box-shadow: 0px 6px 10px #2196F3;
+    border-radius: 5px;
+}
+
+.row{
+    padding: 5px;
+}
+
+fieldset{
+    border-radius: 10px;
+    padding: 20px;
+    margin-bottom: 20px;
+    box-shadow: 1px 1px 2px 2px #E91E63;
+}
+
+legend{
+    font-size: 1.5rem;
+    font-weight: bold;
+    font-family: Verdana;
+}
 `
